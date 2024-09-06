79c643f3e62e7baf4a3df4cae17196c9cc4eaad3
Jovisfe <166058973+Jovisfe@users.noreply.github.com>
2024-09-06 01:10:01 -0300

    Variáveis e constantes
    Primeira publicação sobre desenvolvimento mobile em kotlin

---
+++ Variaveis_Constantes/app/src/main/java/com/example/variaveis_constantes/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.variaveis_constantes
+const val nome = "João" //tempo de compilação
+
+fun main (){
+
+    val nome = "João" //tempo de execução
+
+    println(nome)
+
+    val numero1 = 20
+    val numero2 = 30
+    val resultado = numero1 + numero2
+
+    println(resultado)
+
+}
